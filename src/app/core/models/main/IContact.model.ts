a0229c6ac77dcaeb01d512a2cc509e7914869083
Camilo Castellanos <kml107@hotmail.com>
2025-09-10 21:58:57 -0500

    Ready for connect with srv-bck

--- src/app/core/models/main/IContact.model.ts
+++ src/app/core/models/main/IContact.model.ts
@@ -2,10 +2,12 @@ export interface IContact {
   contactId: number | null;
   username: string | null;
   publicKey: string | null;
+  addedAt: string | null;
 }
 
 export const initialContact: IContact = {
   contactId: null,
   username: null,
   publicKey: null,
+  addedAt: null,
 };
