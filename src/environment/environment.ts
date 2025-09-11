af693eb6340e06841826f9ee8262b3fa33db9512
Camilo Castellanos <kml107@hotmail.com>
2025-09-10 21:54:27 -0500

    Ready for connect with srv

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   isProduction: false,
-  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://cryptomessage-server.onrender.com',
 };
