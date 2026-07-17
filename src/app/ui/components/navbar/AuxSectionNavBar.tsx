55f0ec087c23a1c809014b613fd36caac9bd5d1b
Camilo Castellanos <kml107@hotmail.com>
2026-07-16 21:31:22 -0500

    ready for deploy

--- src/app/ui/components/navbar/AuxSectionNavBar.tsx
+++ src/app/ui/components/navbar/AuxSectionNavBar.tsx
@@ -10,5 +10,4 @@ const StyledAuxSectionNavBar = styled(GenericContainer)`
   flex-direction: row;
   justify-content: space-around;
   width: ${({ theme }) => theme.general.auxSectionWidth};
-  /* border: 1px solid #999999; */
 `;
